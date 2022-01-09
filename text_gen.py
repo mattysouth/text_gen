7a89063ce80c32b259b5bf94c51f0815b68d4b51
mattysouth <52103551+mattysouth@users.noreply.github.com>
2022-01-10 01:28:19 +0900

    Create text_gen.py

---
+++ text_gen.py
@@ -0,0 +1,24 @@
+import requests
+import pyautogui
+import streamlit as st
+st.header("The Problem of Evil")
+st.subheader("This demo uses different ML models to help people think about different shapes of the future.")
+st.text_input()
+
+if pyautogui.press('tab'):
+  finish_text()
+
+def finish_text():
+  text = input()
+    API_URL = "https://api-inference.huggingface.co/models/gpt2"
+    headers = {"Authorization": "Bearer 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"}
+    payload = (text)
+    response = requests.post(API_URL, headers=headers, json=payload)
+    press_tab()
+    st.write(response.json())
+    
+def press_tab():
+    new_text()
+
+def new_text():
+    text = ""
